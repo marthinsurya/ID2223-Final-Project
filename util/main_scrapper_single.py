f3104936455edadd2a85aa24951f33c8213eb5cf
Marthin Surya Setiawan <marthinsurya@gmail.com>
2025-01-10 08:15:13 +0100

    main_scrapper add single mode and traning mode file

---
+++ util/main_scrapper_single.py
@@ -0,0 +1,29 @@
+from Recent_match_scrapper import get_multiple_matches_stats
+from Meta_scrapper import get_meta_stats
+from Leaderboard_scrapper import scrape_leaderboards
+from connection_check import check_connection
+from helper import merge_stats, filter_leaderboard, get_player_list
+from Player_scrapper import get_multiple_player_stats, get_player_stats
+from feature_eng import create_champion_features
+from Weekly_meta_scrapper import get_weekly_meta
+import pandas as pd
+
+meta_stats = get_meta_stats()                       #save to meta_stats.csv
+weekly_meta_stats = get_weekly_meta()               #save to weekly_meta_stats.csv
+
+# Sample data
+data = {
+    'username': ['cat goes meow #silly'],
+    'region': ['vn']
+}
+
+# Create DataFrame
+player_df = pd.DataFrame(data)
+
+
+player_stats = get_multiple_player_stats(player_df)    #save to player_stats.csv
+recent_stats = get_multiple_matches_stats(player_df)   #save to recent_stats.csv
+merged_stats = merge_stats(recent_stats, player_stats)          #save to player_stats_merged.csv
+
+#feature engineering
+training_features = create_champion_features(merged_player_stats=merged_stats, debug=None, consider_team_comp=True, test_mode=False)   #save to feature_eng_stats.csv
